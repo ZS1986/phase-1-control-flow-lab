b7d1d2032ec52d7eb3d59e95fb78305a00e1b8b8
Zehedina Smajlovic <usatransport16@gmail.com>
2022-10-13 10:45:54 -0600

    commit tree 78191eb5ed386187ba3010381d7b2e969bbe5195

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet (someValue){
+  let freeRide = 'This one is on me!';
+  if (someValue <= 400){
+    return freeRide;
+  }
+  else if (someValue <= 2000){
+    return 'That will be twenty bucks.';
+  }
+    else if (someValue <=2500){
+      return 'I will gladly take your thirty bucks.';
+    }
+    else{
+      return 'No can do.';
+    }
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(value){
+  return value === 'NYC' ? "Ok, sounds good." : "No go." ;
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(greeting){
+  switch (greeting){
+    case 'generous':
+      return "Thank you so much.";
+      case 'not as generous':
+        return "Thank you.";
+      default:
+        return "Bye." 
+       }
 }
